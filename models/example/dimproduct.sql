d093a6c7ebddf61cb329ba53bb5c70d6e5b76582
vignesh.dhandapani@happiestminds.com <vignesh.dhandapani@happiestminds.com>
2022-06-23 15:02:59 +0000

    newjun23

---
+++ models/example/dimproduct.sql
@@ -0,0 +1,22 @@
+{{ config(materialized='incremental') }}
+
+select 
+
+PRODUCT_ID AS PRODUCTID,
+ID AS PRODUCTSOURCEKEY,
+PRODUCT_NAME AS PRODUCTNAME,
+SEGMENT AS SEGMENT,
+SUB_CATEGORY AS SUBCATEGORY,
+CATEGORY AS CATEGORY,
+_FIVETRAN_SYNCED AS CREATEDDATE,
+'fivetran' AS CREATEDBY,
+ current_timestamp AS MODIFIEDDATE,
+'fivetran' AS MODIFIEDBY
+
+from "DATA_TO_INSIGHTS"."DATA_TO_INSIGHTS_TRAINING"."STG_DIMPRODUCT"
+
+{% if is_incremental() %}
+  -- this filter will only be applied on an incremental run
+  where to_date(_FIVETRAN_SYNCED) > (select max(to_date(_FIVETRAN_SYNCED)) from {{ this }})
+{% endif %}
+
